339e2f9bac58dc1ca2e0d6588abadb51ba50d226
Shorya Malani <shoryamal@gmail.com>
2022-07-09 21:18:49 +0530

    fixing index.js

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
 $('#twitter-button').on('click', function() {
     // Initialize with your OAuth.io app public key
-    OAuth.initialize('gZeK0rjdjMpH70JACRM_kaKLUIc');
+    OAuth.initialize('vNh6GPQ75Fwe9bCaSkcUSbXuS');
     // Use popup for OAuth
     OAuth.popup('twitter').then(twitter => {
       console.log(twitter);
